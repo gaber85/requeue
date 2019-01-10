121e277cc7598ca629497085d7e640b1aca37549
gaber85 <gabrielr85@gmail.com>
2019-01-10 10:28:30 +0100

    CSS changes made to sign-in-component

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
-import './Stylesheets/App.css';
-import './Stylesheets/Login.css';
+import React, { Component } from "react";
+import "./Stylesheets/App.css";
+import "./Stylesheets/Login.css";
 
-import ConnectedUsers from './Host-components/connected-users';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Dashboard from './Host-components/dashboard-component';
-import History from './Host-components/history-component';
-import NavBar from './Host-components/nav-bar-component';
-import LandingPage from './Login-components/landing-page-component';
-import SignInPage from './Login-components/sign-in-page-component';
+import ConnectedUsers from "./Host-components/connected-users";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Dashboard from "./Host-components/dashboard-component";
+import History from "./Host-components/history-component";
+import NavBar from "./Host-components/nav-bar-component";
+import LandingPage from "./Login-components/landing-page-component";
+import SignInPage from "./Login-components/sign-in-page-component";
 
 class App extends Component {
-
   render() {
     return (
-      <SignInPage />
-        // <Router>
-        //   <div className="main-container">
-        //   <NavBar />
-        //  <Route exact path="/" component={Dashboard} />
-        //  <Route path="/users" component={ConnectedUsers} />
-        //  <Route path="/history" component={History} />
-        //  </div>
-        // </Router>
+      <Router>
+        <div className="main-container">
+          <NavBar />
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/users" component={ConnectedUsers} />
+          <Route path="/history" component={History} />
+        </div>
+      </Router>
     );
   }
 }
--- client/src/Login-components/sign-in-page-component.js
+++ client/src/Login-components/sign-in-page-component.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import Logo from './White.png';
 
 class SignInPage extends Component {
+  white = '#FFFFFF';
   render () {
     return (
       <div className="landing-page-container">
         <img className="logo" alt="logo" src={Logo}></img>
-        <form className="inputs">
+        <form className="form-inputs">
           <input className="sign-in-input" type="text" placeholder="Username or Email..."></input>
           <input className="sign-in-input" type="text" placeholder="Password..."></input>
-          <input className="checkbox" type="checkbox"></input>
-          <button type="submit" className="sign-in-button small-button">Sign In</button>
+          <div>
+          <input className="checkbox" type="checkbox" placeholder="Remember Me"></input>
+          <label style={{color:this.white}}>Remember me</label>
+          </div>
+          <button type="submit" className="small-button">Sign In</button>
         </form>
       </div>
     );
