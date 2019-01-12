4d5e9111f6a87afe48cfdd7aead5a57dfb184d29
gaber85 <gabrielr85@gmail.com>
2019-01-12 15:58:16 +0100

    server redirect after login now goes to intro page component

--- client/src/Login-components/intro-page-component.js
+++ client/src/Login-components/intro-page-component.js
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 class IntroPage extends Component {
   render () {
     return (
-      <div className="landing-page-container">
-        <img className="small-logo" alt="logo" src={Logo}></img>
+      <div className="intro-page-container">
+        {/* <img className="small-logo" alt="logo" src={Logo}></img> */}
         <div className="intro-title">I want to...</div>
         <div className="intro-button"><Link className="black-link" to="/host">Host</Link></div>
         <div className="intro-button">Join</div>
