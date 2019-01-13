7b58589d013605370345416fe3b69db622a9c181
gaber85 <gabrielr85@gmail.com>
2019-01-13 14:16:03 +0100

    minor css and code cleaning changes

--- client/src/Login-components/intro-page-component.js
+++ client/src/Login-components/intro-page-component.js
@@ -1,39 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class IntroPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      welcome: true,
-    }
+      welcome: true
+    };
     this.welcomeInterval = null;
   }
 
   handleWelcome() {
     this.setState({
-      welcome: false,
-    })
+      welcome: false
+    });
   }
 
   componentWillMount() {
     setTimeout(() => this.handleWelcome(), 2000);
   }
 
-  render () {
+  render() {
     const { welcome } = this.state;
     return (
       <div className="intro-page-container">
-      {welcome ? 
-          (<div className="animated pulse duration-2s welcome-title">Welcome</div>)
-      :
-        (
-        <div className="animated fadeIn duration-3s intro-page-container">
-          <div className="intro-title">I want to...</div>
-          <div className="intro-button"><Link className="black-link" to="/host">Host</Link></div>
-          <div className="intro-button">Join</div>
-        </div>)
-        }
+        {welcome ? (
+          <div className="animated pulse duration-2s welcome-title">
+            Welcome
+          </div>
+        ) : (
+          <div className="animated fadeIn duration-3s intro-page-container">
+            <div className="intro-title">I want to...</div>
+            <div className="intro-button">
+              <Link className="black-link" to="/host">Host</Link>
+            </div>
+            <div className="intro-button">Join</div>
+          </div>
+        )}
       </div>
     );
   }
--- client/src/Login-components/landing-page-component.js
+++ client/src/Login-components/landing-page-component.js
@@ -1,34 +1,18 @@
-import React, { Component } from 'react';
-import Logo from './White.png';
-// import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import Logo from "./White.png";
 
 class LandingPage extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     token: '',
-  //     deviceId: '',
-  //     loggedIn: false,
-  //     error: '',
-  //     trackName: '',
-  //     artistName: '',
-  //     albumName: '',
-  //     playing: false,
-  //     position: 0,
-  //     duration: 0,
-  //   }
-  // }
 
-  BASE_URL = 'http://localhost:3001/login';
+  BASE_URL = "http://localhost:3001/login";
 
-
-  render () {
+  render() {
     return (
-      <div className="landing-page-container">
-        <img className="logo" alt="logo" src={Logo}></img>
+      <div className="animated fadeIn delay-1s duration-2s landing-page-container">
+        <img className="logo" alt="logo" src={Logo} />
         <div className="white-text">Sign in with Spotify</div>
-        <div className="sign-in-button"><a className="white-link" href={this.BASE_URL}>Sign In</a></div>
-        {/* <div className="sign-up-button"><Link className="black-link" to='/register'>Sign Up</Link></div> */}
+        <div className="sign-in-button">
+          <a className="white-link" href={this.BASE_URL}>Sign In</a>
+        </div>
       </div>
     );
   }
